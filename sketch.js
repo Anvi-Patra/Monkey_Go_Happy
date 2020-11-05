ea958ffb4783df4d8740294fbddf224e9b258fe9
Anvi-Patra <73284497+Anvi-Patra@users.noreply.github.com>
2020-11-05 23:54:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var monkey , monkey_running;
+var banana, bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup;
+var score=0;
+var survivalTime=0;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+}
+
+
+
+function setup() {
+createCanvas(400,380);
+
+  monkey=createSprite(60,335,20,20);
+monkey.addAnimation("moving",monkey_running);
+monkey.scale=0.1;
+  
+ground=createSprite(400,370,900,40);
+ground.velocityX=-4;
+
+FoodGroup = new Group();
+obstacleGroup = new Group();
+}
+
+
+function draw() {
+  background(37, 82, 6);
+  text("Score= "+score,340,20)
+  survivalTime=Math.ceil(frameCount/frameRate());
+  text("Survival Time= "+survivalTime,220,20);
+  
+          
+    if(keyDown("space")){
+      monkey.velocityY=-12;
+    }
+    
+    monkey.velocityY=monkey.velocityY+0.8;
+    monkey.collide(ground);
+    
+    if(ground.x<0){
+      ground.x=ground.width/2;
+    }
+    
+    console.log(ground.x);
+    spawnBanana();
+    spawnObstacles();
+   
+    if (FoodGroup.isTouching(monkey)){
+      FoodGroup.destroyEach();
+      score=score+2;
+    }
+
+    if (obstacleGroup.isTouching(monkey)){
+      text("Oops the monkey was caught",140,200);
+      obstacleGroup.destroyEach();
+      FoodGroup.destroyEach();
+      ground.velocityX = 0;
+      monkey.destroyEach();
+      
+    }
+  
+  drawSprites(); 
+}
+
+function spawnBanana(){
+ if(frameCount % 80 === 0){
+  banana=createSprite(400,180,10,10);
+  banana.velocityX=-5;
+  banana.y=Math.round(random(140,200));
+  banana.addImage(bananaImage);
+  banana.scale=0.1;
+  banana.lifetime=150;
+  FoodGroup.add(banana);
+ }  
+}
+
+function spawnObstacles(){
+  if(frameCount % 300 === 0){
+    obstacle=createSprite(400,320,10,10);
+    obstacle.velocityX=-7;
+    obstacle.addImage(obstacleImage);
+    obstacle.scale=0.15;
+    obstacle.lifetime=150;
+    obstacleGroup.add(obstacle);
+  }
+}
+
+
+
+
